e16b0e491039f7aa54221034a3ca845fce4019a8
Emre Yavuz <Emre@emres-macbook-air.local>
2016-07-20 12:35:06 +0300

    Initial Commit

---
+++ RNAssetThumbnail/RNAssetThumbnail.h
@@ -0,0 +1,13 @@
+//
+//  RNAssetThumbnail.h
+//  RNAssetThumbnail
+//
+//  Created by Emre Yavuz on 20.07.2016.
+//  Copyright © 2016 Codeventure. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNAssetThumbnail : NSObject
+
+@end
---
+++ RNAssetThumbnail/RNAssetThumbnail.m
@@ -0,0 +1,13 @@
+//
+//  RNAssetThumbnail.m
+//  RNAssetThumbnail
+//
+//  Created by Emre Yavuz on 20.07.2016.
+//  Copyright © 2016 Codeventure. All rights reserved.
+//
+
+#import "RNAssetThumbnail.h"
+
+@implementation RNAssetThumbnail
+
+@end
